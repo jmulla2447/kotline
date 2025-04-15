78f1f013f59e9d08d78c1179a25a7911e653fd3e
javedmulla <javed.java18@gmail.com>
2025-04-15 21:23:15 +0200

    observable	       vs      vetoable

--- smart/cast/delegated/observable/User.kt
+++ smart/cast/delegated/observable/User.kt
@@ -3,9 +3,13 @@ package smart.cast.delegated.observable
 import kotlin.properties.Delegates
 
 class User {
-    var name: String by Delegates.vetoable("Nulll") {
-            prop, old, new ->
+    var name: String by Delegates.observable("-") {
+            _, old, new ->
         println("$old -> $new")
+    }
+
+    var age : Int by Delegates.vetoable(11){
+        _, _, newValue -> newValue >=18
     }
 }
 
@@ -13,4 +17,14 @@ fun main() {
     val user = User()
     user.name = "first"
     user.name = "second"
+    user.age = 15
+    println(user.age)
 }
+
+/*
+*   Feature	                observable	            vetoable
+    When it's called	    After the value is set	Before the value is set
+    Can stop update?	    ❌ No	                ✅ Yes (by returning false)
+    Return value needed?	No (returns Unit)	    Yes (Boolean - allow or block)
+    Typical use	Logging,    reacting to changes	    Validation, conditional update
+* */
